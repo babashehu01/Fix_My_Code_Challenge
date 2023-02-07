9b16cf36653928e30d191e9258e33565ed1a8b8d
Babashehu Shettima Musti <shettimababashehu57@gmail.com>
2023-02-07 07:31:43 +0100

    Fixed height in Square class

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python3
 
-class Square():
+class square():
     
-    side = 0
+    width = 0
+    height = 0
 
-    def __init__(self, side, *args, **kwargs):
-        self.side = side
+    
+    def __init__(self, *args, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.side * self.side
+        return self.width * self.height
 
-    def PerimeterOfMySquare(self):
-        return self.side * 4
+    def PermiterOfMySquare(self):
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        return "{}".format(self.side)
+        return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
 
-    s = Square(12)
+    s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PerimeterOfMySquare())
+    print(s.PermiterOfMySquare())
